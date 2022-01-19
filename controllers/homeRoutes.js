d6f309d186870558f6bf7536db863289c701fc64
Bonnie Reed <bonniejeanreed@gmail.com>
2022-01-18 22:45:25 -0600

    re-wrote homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,137 +1,80 @@
 const router = require('express').Router();
-const { Comment, User, Post } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-//hompage rendering
 router.get('/', async (req, res) => {
     try {
-        // render homepage
-        res.render('homepage', {
-            logged_in: req.session.logged_in,
-        });
-        console.log(req.session);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-
-// renders all comments
-router.get('/post', async (req, res) => {
-    try {
         const postData = await Post.findAll({
-            where: {
-                user_id: null,
-            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
         });
-        const post = postData.map((post) =>
-            post.get({ plain: true })
-        );
-
-        res.render('post', {
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render('homepage', {
             posts,
-            logged_in: req.session.logged_in,
+            logged_in: req.session.logged_in
         });
     } catch (err) {
         res.status(500).json(err);
     }
 });
-//render by individual comment
-router.get('/post/:id', async (req, res) => {
+
+router.get('/posts/:id', async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
             include: [
                 {
                     model: User,
-                    attributes: ['name'],
+                    attributes: ['username'],
                 },
             ],
         });
 
         const post = postData.get({ plain: true });
-
-        res.render('post', {
-            post,
-            logged_in: req.session.logged_in,
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-// renders all comments
-router.get('/comment', async (req, res) => {
-    try {
         const commentData = await Comment.findAll({
             where: {
-                user_id: null,
+                post_id: req.params.id,
             },
-        });
-        const comments = commentData.map((comment) =>
-            comment.get({ plain: true })
-        );
-
-        res.render('comment', {
+            include: [{
+                model: User,
+                attributes: ['username']
+            }]
+        })
+        let comments = commentData.map((comment) => comment.get({ plain: true }))
+        res.render('post', {
+            post,
             comments,
-            logged_in: req.session.logged_in,
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-//render by individual comment
-router.get('/comment/:id', async (req, res) => {
-    try {
-        const commentData = await Comment.findByPk(req.params.id, {
-            include: [
-                {
-                    model: User,
-                    attributes: ['name'],
-                },
-            ],
-        });
-
-        const comment = commentData.get({ plain: true });
-
-        res.render('comment', {
-            comment,
-            logged_in: req.session.logged_in,
+            logged_in: req.session.logged_in
         });
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
     try {
-        console.log(req.session.user_id);
-        // Find the logged in trainer based on the session ID
         const userData = await User.findByPk(req.session.user_id, {
             attributes: { exclude: ['password'] },
-            include: [{ model: Comment }],
+            include: [{ model: Post }],
         });
 
         const user = userData.get({ plain: true });
-        const { comments } = user;
-        // console.log(trainer);
-        console.log(comments);
-        res.render('profile', {
-            user,
-            comments,
-            logged_in: true,
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
         });
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-// render login screen
 router.get('/login', (req, res) => {
-    console.log('this is inside of login route');
-    console.log(req.session);
-    // If the trainer is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-        res.redirect('/profile');
+        res.redirect('/dashboard');
         return;
     }
 
